cf058960a421c81aaa509d318de134a5253ac715
bhr94 <61164000+bhr94@users.noreply.github.com>
2020-08-26 12:28:57 -0700

    Create new pages like Signup and Signin and used bootstrap and some already written codes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import SigninPage from "./Pages/SigninPage"
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div>
+      <SigninPage/>
     </div>
   );
 }
